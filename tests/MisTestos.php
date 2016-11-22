d00ecf6d76a56dd3b440491661f24911573a26d3
Marc <mcalafellsmx@gmail.com>
2016-11-22 16:17:30 +0100

    added a test

---
+++ tests/MisTestos.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mark
+ * Date: 22/11/16
+ * Time: 16:15
+ */
+
+
+class MisTestos
+{
+
+    /**
+     * prova
+     */
+    public function prova() {
+        echo "asd";
+    }
+
+}
